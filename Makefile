de968eeb372316920f50f5bcab1121e6f19a97ee
Khouloud BOUZEKI YENGUI <khouloud.yengui@exakis-nelite.com>
2025-03-10 10:35:23 +0100

    FIX WORKFLOW

---
+++ Makefile
@@ -0,0 +1,36 @@
+NAME := databricks_starter_kit
+POETRY := $(shell command -v poetry 2> /dev/null)
+
+.DEFAULT_GOAL := help
+
+.PHONY: help
+help:
+	@echo "Please use 'make <target>' where <target> is one of"
+	@echo ""
+	@echo "  install     		install packages and prepare environment"
+	@echo "  lint        		run the code linters"
+	@echo "  format      		reformat code"
+	@echo "  tests        		run all the tests"
+	@echo "  tests-with-coverage	run all the tests with coverage results"
+	@echo ""
+	@echo "Check the Makefile to know exactly what each target is doing."
+
+install: pyproject.toml poetry.lock
+	@if [ -z $(POETRY) ]; then echo "Poetry could not be found. See https://python-poetry.org/docs/"; exit 2; fi
+	$(POETRY) install
+
+.PHONY: lint
+lint:
+	$(POETRY) run nox --session="lint"
+
+.PHONY: format
+format:
+	$(POETRY) run nox --session="format"
+
+.PHONY: tests
+tests:
+	$(POETRY) run nox --session="tests"
+
+.PHONY: tests-with-coverage
+tests-with-coverage:
+	$(POETRY) run nox --session="tests_with_coverage"
---
+++ noxfile.py
@@ -0,0 +1,37 @@
+import nox
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    session.run("poetry", "run", "pytest", "tests/unit", external=True)
+
+
+@nox.session
+def tests_with_coverage(session: nox.Session) -> None:
+    session.run(
+        "poetry", "run", "pytest", "tests", "tests/unit", "--cov=.", "--cov-report", "xml:coverage.xml", external=True
+    )
+
+
+@nox.session
+def format(session: nox.Session) -> None:
+    session.run("poetry", "run", "ruff", "format", ".", external=True)
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    session.run("poetry", "run", "ruff", "check", "databricks_starter_kit", "--fix", external=True)
+    session.run("poetry", "run", "ruff", "check", "databricks_starter_kit", external=True)
+    session.run("poetry", "run", "ruff", "check", "tests", "--fix", external=True)
+    session.run("poetry", "run", "ruff", "check", "tests", external=True)
+    mypy(session)
+
+
+@nox.session
+def mypy(session: nox.Session) -> None:
+    session.run("poetry", "run", "mypy", ".", "--install-types", "--non-interactive", external=True)
+
+
+@nox.session
+def security(session: nox.Session) -> None:
+    session.run("bash", "-c", "poetry export -f requirements.txt | skjold -v audit -", external=True)
